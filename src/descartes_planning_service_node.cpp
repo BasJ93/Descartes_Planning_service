e8854fd891f4b5cead623160f4130e3190e9823d
Bas Janssen <smh.janssen@gmail.com>
2018-01-29 12:34:16 +0100

    Added path visualization to the service. Added interpolation as a parameter. Added joint_state message as part of the request to seed the planner to the pose of the robot. This avoids the robot changing its pose because starting from a different pose has a lower cost.

--- src/descartes_planning_service_node.cpp
+++ src/descartes_planning_service_node.cpp
@@ -10,8 +10,11 @@
 // Includes the descartes trajectory type we will be using
 #include <descartes_trajectory/axial_symmetric_pt.h>
 #include <descartes_trajectory/cart_trajectory_pt.h>
+#include <descartes_trajectory/joint_trajectory_pt.h>
 // Includes the planner we will be using
 #include <descartes_planner/dense_planner.h>
+
+#include <visualization_msgs/Marker.h>
 
 //The path is build using points in the eigen convention
 #include <eigen_conversions/eigen_msg.h>
@@ -25,6 +28,9 @@ typedef TrajectoryVec::const_iterator TrajectoryIter;
 
 //There is a cleaner way, but this works...
 std::vector<std::string> names;
+ros::Publisher marker_publisher_;
+
+void publishPosesMarkers(const EigenSTL::vector_Affine3d& poses, std::string WORLD_FRAME, float AXIS_LINE_WIDTH, float AXIS_LINE_LENGHT);
 
 //Convert the poseArray to Eigen
 EigenSTL::vector_Affine3d toEigenArray(const geometry_msgs::PoseArray &poses)
@@ -39,22 +45,22 @@ EigenSTL::vector_Affine3d toEigenArray(const geometry_msgs::PoseArray &poses)
 }
 
 //Should make axis and interpolation parameters.
-descartes_core::TrajectoryPtPtr makeTolerancedCartesianPoint(const Eigen::Affine3d& pose, std::string underdefinedAxis)
+descartes_core::TrajectoryPtPtr makeTolerancedCartesianPoint(const Eigen::Affine3d& pose, std::string underdefinedAxis, float interpolation)
 {
   using namespace descartes_core;
   using namespace descartes_trajectory;
 
   if(underdefinedAxis.compare("X_AXIS") == 0)
   {
-    return TrajectoryPtPtr( new AxialSymmetricPt(pose, M_PI/30, AxialSymmetricPt::X_AXIS) );
+    return TrajectoryPtPtr( new AxialSymmetricPt(pose, interpolation, AxialSymmetricPt::X_AXIS) );
   }
   else if(underdefinedAxis.compare("Y_AXIS") == 0)
   {
-    return TrajectoryPtPtr( new AxialSymmetricPt(pose, M_PI/30, AxialSymmetricPt::Y_AXIS) );
+    return TrajectoryPtPtr( new AxialSymmetricPt(pose, interpolation, AxialSymmetricPt::Y_AXIS) );
   }
   else if(underdefinedAxis.compare("Z_AXIS") == 0)
   {
-    return TrajectoryPtPtr( new AxialSymmetricPt(pose, M_PI/30, AxialSymmetricPt::X_AXIS) );
+    return TrajectoryPtPtr( new AxialSymmetricPt(pose, interpolation, AxialSymmetricPt::Z_AXIS) );
   }
 }
 
@@ -104,12 +110,16 @@ toROSJointTrajectory(const TrajectoryVec& trajectory,
 bool generate_motion_plan(descartes_planning_service::generate_motion_plan::Request &req, descartes_planning_service::generate_motion_plan::Response &res)
 {
   EigenSTL::vector_Affine3d points = toEigenArray(req.path);
+  publishPosesMarkers(points, req.world_frame, 0.01, 0.01);
   
   TrajectoryVec path;
+  
+  path.push_back(descartes_core::TrajectoryPtPtr( new descartes_trajectory::JointTrajectoryPt(req.joint_states.position) ));
+  
   for(unsigned int i = 0; i < points.size(); i++)
   {
     const Eigen::Affine3d& pose = points[i];
-    descartes_core::TrajectoryPtPtr pt = makeTolerancedCartesianPoint(pose, req.underdefinedAxis);
+    descartes_core::TrajectoryPtPtr pt = makeTolerancedCartesianPoint(pose, req.underdefinedAxis, req.interpolation);
     path.push_back(pt);
   }
     
@@ -145,7 +155,7 @@ bool generate_motion_plan(descartes_planning_service::generate_motion_plan::Requ
 
   TrajectoryVec result;
 
-  ROS_INFO("Startong path planning");
+  ROS_INFO("Starting path planning");
 
   clock_t t;
   t = clock();
@@ -185,6 +195,101 @@ bool generate_motion_plan(descartes_planning_service::generate_motion_plan::Requ
   }
 }
 
+//Publish the path visualization to rviz
+void publishPosesMarkers(const EigenSTL::vector_Affine3d& poses, std::string WORLD_FRAME, float AXIS_LINE_WIDTH, float AXIS_LINE_LENGHT)
+{
+  // creating rviz markers
+  visualization_msgs::Marker z_axes, y_axes, x_axes, line;
+  visualization_msgs::MarkerArray markers_msg;
+
+  z_axes.type = y_axes.type = x_axes.type = visualization_msgs::Marker::LINE_LIST;
+  z_axes.ns = y_axes.ns = x_axes.ns = "axes";
+  z_axes.action = y_axes.action = x_axes.action = visualization_msgs::Marker::ADD;
+  z_axes.lifetime = y_axes.lifetime = x_axes.lifetime = ros::Duration(0);
+  z_axes.header.frame_id = y_axes.header.frame_id = x_axes.header.frame_id = WORLD_FRAME;
+  z_axes.scale.x = y_axes.scale.x = x_axes.scale.x = AXIS_LINE_WIDTH;
+
+  // z properties
+  z_axes.id = 0;
+  z_axes.color.r = 0;
+  z_axes.color.g = 0;
+  z_axes.color.b = 1;
+  z_axes.color.a = 1;
+
+  // y properties
+  y_axes.id = 1;
+  y_axes.color.r = 0;
+  y_axes.color.g = 1;
+  y_axes.color.b = 0;
+  y_axes.color.a = 1;
+
+  // x properties
+  x_axes.id = 2;
+  x_axes.color.r = 1;
+  x_axes.color.g = 0;
+  x_axes.color.b = 0;
+  x_axes.color.a = 1;
+
+  // line properties
+  line.type = visualization_msgs::Marker::LINE_STRIP;
+  line.ns = "line";
+  line.action = visualization_msgs::Marker::ADD;
+  line.lifetime = ros::Duration(0);
+  line.header.frame_id = WORLD_FRAME;
+  line.scale.x = AXIS_LINE_WIDTH;
+  line.id = 0;
+  line.color.r = 1;
+  line.color.g = 1;
+  line.color.b = 0;
+  line.color.a = 1;
+
+  // creating axes markers
+  z_axes.points.reserve(2*poses.size());
+  y_axes.points.reserve(2*poses.size());
+  x_axes.points.reserve(2*poses.size());
+  line.points.reserve(poses.size());
+  geometry_msgs::Point p_start,p_end;
+  double distance = 0;
+  Eigen::Affine3d prev = poses[0];
+  for(unsigned int i = 0; i < poses.size(); i++)
+  {
+    const Eigen::Affine3d& pose = poses[i];
+    distance = (pose.translation() - prev.translation()).norm();
+
+    tf::pointEigenToMsg(pose.translation(),p_start);
+
+    if(distance > 0.01)
+    {
+      Eigen::Affine3d moved_along_x = pose * Eigen::Translation3d(AXIS_LINE_LENGHT,0,0);
+      tf::pointEigenToMsg(moved_along_x.translation(),p_end);
+      x_axes.points.push_back(p_start);
+      x_axes.points.push_back(p_end);
+
+      Eigen::Affine3d moved_along_y = pose * Eigen::Translation3d(0,AXIS_LINE_LENGHT,0);
+      tf::pointEigenToMsg(moved_along_y.translation(),p_end);
+      y_axes.points.push_back(p_start);
+      y_axes.points.push_back(p_end);
+
+      Eigen::Affine3d moved_along_z = pose * Eigen::Translation3d(0,0,AXIS_LINE_LENGHT);
+      tf::pointEigenToMsg(moved_along_z.translation(),p_end);
+      z_axes.points.push_back(p_start);
+      z_axes.points.push_back(p_end);
+
+      // saving previous
+      prev = pose;
+    }
+
+    line.points.push_back(p_start);
+  }
+
+  markers_msg.markers.push_back(x_axes);
+  markers_msg.markers.push_back(y_axes);
+  markers_msg.markers.push_back(z_axes);
+  markers_msg.markers.push_back(line);
+
+  marker_publisher_.publish(markers_msg);
+
+}
 
 int main(int argc, char** argv)
 {
@@ -194,8 +299,10 @@ int main(int argc, char** argv)
   
   // Get Joint Names
   nh.getParam("controller_joint_names", names);
+  marker_publisher_ = nh.advertise<visualization_msgs::MarkerArray>("visualize_trajectory_curve",1,true);
   
   ros::ServiceServer ur_generate_motion_plan_server = nh.advertiseService("descartes_generate_motion_plan", generate_motion_plan);
+  //ros::ServiceServer visualize_trajectory_server = nh.advertiseService("visualize_trajectory", visualize_trajectory);
   ROS_INFO("Motion planner service online");
   ros::spin();
   
